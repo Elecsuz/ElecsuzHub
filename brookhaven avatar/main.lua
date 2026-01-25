e67a29c109858968d12e737f8b789079241ebe91
Elecsuz <456321yucelkerem@gmail.com>
2026-01-25 16:45:03 +0300

    Update main.lua

--- brookhaven avatar/main.lua
+++ brookhaven avatar/main.lua
@@ -1,9 +1,9 @@
 -- ELECSUZ HUB - MASTER LOADER
 local function load(file)
-    local url = "https://raw.githubusercontent.com/Elecsuz/ElecsuzHub/main/" .. file
+    local url = https://raw.githubusercontent.com/Elecsuz/ElecsuzHub/refs/heads/main/brookhaven%20avatar/" .. file
     loadstring(game:HttpGet(url))()
 end
 
-load("security.lua") -- Önce güvenlik
-load("admin.lua")    -- Sonra yetkiler
-load("ui_tabs.lua")  -- En son arayüz
+load("security.lua")
+load("admin.lua")
+load("ui_tabs.lua")
