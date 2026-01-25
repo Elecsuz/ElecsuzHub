61d1538d0989aec8335ba5e41e9ca44e42b1a7cc
Elecsuz <456321yucelkerem@gmail.com>
2026-01-25 18:48:06 +0300

    Update loader.lua

--- brookhaven avatar/loader.lua
+++ brookhaven avatar/loader.lua
@@ -1,14 +1,18 @@
 local function loadModule(file)
     local url = "https://raw.githubusercontent.com/Elecsuz/ElecsuzHub/refs/heads/main/brookhaven%20avatar/" .. file
     
-    pcall(function()
+    pcall(function() 
         local content = game:HttpGet(url)
         if content then
-            loadstring(content)()
+            local func = loadstring(content)
+            if func then
+                func()
+            end
         end
     end)
 end
 
 loadModule("security.lua")
+loadModule("functions.lua")
 loadModule("admin.lua")
 loadModule("ui_tabs.lua")
